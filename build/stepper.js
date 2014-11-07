e467fd992447c139763fa12e70f7876e819d6d9c
Kevin Barabash <kevinb7@gmail.com>
2014-11-07 11:32:33 -0700

    moved scope with the other private functions, temporarily removing comments until refactoring is finished, modified _runScope to call _popAndStoreYieldValue

--- build/stepper.js
+++ build/stepper.js
@@ -613,11 +613,6 @@ Stepper.prototype.halted = function () {
     return this.done;
 };
 
-/**
- * Primitive step function
- * @returns {*} generator result or undefined
- * @private
- */
 Stepper.prototype._step = function () {
     if (this.stack.isEmpty()) {
         this.done = true;
@@ -629,32 +624,37 @@ Stepper.prototype._step = function () {
     return result;
 };
 
-/**
- * 
- * @param value value to store in this.yieldVal
- * @returns {*}
- * @private
- */
+Stepper.prototype._runScope = function (frame) {
+    this.stack.push(frame);
+
+    var result = this._step();
+    while (!result.done) {
+        if (result.value.gen) {
+            this._runScope(result.value);
+        }
+        result = this._step();
+    }
+    
+    this._popAndStoreYieldValue(result.value);
+};
+
 Stepper.prototype._popAndStoreYieldValue = function (value) {
     var frame = this.stack.pop();
-    if (frame.gen.obj) {
-        this.yieldVal = frame.gen.obj;
-    } else {
-        this.yieldVal = value;
-    }
-    return frame.lineno;
+    this.yieldVal = frame.gen.obj || value;
+    return frame;
 };
 
 Stepper.prototype.stepIn = function () {
     var result;
     if (result = this._step()) {
         if (result.done) {
-            return this._popAndStoreYieldValue(result.value);
+            var frame = this._popAndStoreYieldValue(result.value);
+            return frame.lineno;
         } else if (result.value.gen) {
             this.stack.push(result.value);
             return this.stepIn();
         }
-        return result.value && result.value.lineno;
+        return result.value.lineno;
     }
 };
 
@@ -662,12 +662,13 @@ Stepper.prototype.stepOver = function () {
     var result;
     if (result = this._step()) {
         if (result.done) {
-            return this._popAndStoreYieldValue(result.value);
+            var frame = this._popAndStoreYieldValue(result.value);
+            return frame.lineno;
         } else if (result.value.gen) {
             this._runScope(result.value);
             return this.stepOver();
         }
-        return result.value && result.value.lineno;
+        return result.value.lineno;
     }
 };
 
@@ -680,7 +681,8 @@ Stepper.prototype.stepOut = function () {
             }
             result = this._step();
         }
-        return this._popAndStoreYieldValue(result.value);
+        var frame = this._popAndStoreYieldValue(result.value);
+        return frame.lineno;
     }
 };
 
@@ -693,25 +695,6 @@ Stepper.prototype.run = function () {
     }
     this.done = true;
     return lineno;
-};
-
-Stepper.prototype._runScope = function (frame) {
-    if (!frame.gen) {
-        throw "not a valid frame";
-    }
-
-    this.stack.push(frame);
-
-    var result = this._step();
-    while (!result.done) {
-        if (result.value.gen) {
-            this._runScope(result.value);
-        }
-        result = this._step();
-    }
-
-    this.yieldVal = frame.gen.obj ? frame.gen.obj : result.value;
-    this.stack.pop();
 };
 
 Stepper.prototype.setBreakpoint = function (lineno) {
