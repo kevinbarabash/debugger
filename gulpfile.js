37f6889b22f7be302ccd0f206d0b7b96e661d393
Kevin Barabash <kevinb7@gmail.com>
2014-11-17 11:30:17 -0700

    first attempt at automating testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var rename = require("gulp-rename");
 var concat = require("gulp-concat");
+var testee = require("testee");
 
 var deps = [
     "./bower_components/esprima/esprima.js",
@@ -30,6 +31,15 @@ gulp.task("build-standalone", ["build"], function () {
 
 gulp.task("watch", function() {
     gulp.watch(src, ["build"]);
+});
+
+gulp.task("test", ["build"], function() {
+    testee.test(["test/runner.html"], ["firefox"], { browsers: "firefox" })
+        .then(function() {
+            process.exit(0);
+        }, function() {
+            process.exit(1);
+        });
 });
 
 gulp.task("default", ["build", "watch"]);
